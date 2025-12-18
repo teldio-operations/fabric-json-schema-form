fb6aec32fbe54620161ad53546178f5629cd5e82
James Kitamirike <jamesk@teldio.com>
2025-12-18 14:17:38 -0500

    Add number as string field

---
+++ components/NumberFieldStringValue.tsx
@@ -0,0 +1,22 @@
+import type { WidgetProps } from "@rjsf/utils";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import NumberField from "./NumberField";
+dayjs.extend(customParseFormat);
+
+export const NumberFieldStringValue = (props: WidgetProps) => {
+    const { label, value, onChange, disabled, readonly, rawErrors } = props;
+
+    const handleChange = (newValue: number | null) => {
+        onChange(newValue !== null ? String(newValue) : undefined);
+    }
+
+    return (<NumberField
+        label={label}
+        value={Number.isNaN(Number(value)) ? null : Number(value)}
+        onValueChange={(value) => handleChange(value)}
+        disabled={disabled || readonly}
+        error={rawErrors && rawErrors.length > 0}
+    />
+    );
+}
