84753bc8ccbd6b148e1aad47aafdc77c4deeed40
Asil Erturan <aerturan@teldio.com>
2026-03-12 14:39:11 -0400

    Move to another file

--- Form/QueryableField.tsx
+++ Form/QueryableField.tsx
@@ -4,8 +4,9 @@ import createFetchClient from "openapi-fetch";
 import createClient from "openapi-react-query";
 import { useMemo } from "react";
 import { LoadingTextField } from "../components/LoadingTextField";
-import type { paths, Queryable } from "../manager-api";
+import type { paths } from "../manager-api";
 import { FabricJsonSchemaForm } from "./Form";
+import { filterQueries } from "./mediaType";
 
 const isObject = (value: unknown): value is Record<string, unknown> => {
   return typeof value === "object" && value !== null && !Array.isArray(value);
@@ -48,72 +49,6 @@ type QueryRequest = {
 
 type QueryableSchema = RJSFSchema & {
   accept?: string;
-};
-
-const notNullOrUndefined = <T,>(value: T): value is NonNullable<T> => {
-  return value !== null && value !== undefined;
-};
-
-const parseAccept = (accept?: string) => {
-  return (
-    accept
-      ?.split(",")
-      .map((p) => p.trim())
-      .map((i) => i.split(";"))
-      .map(([baseType, ...parameters]) =>
-        [baseType?.split("/", 2), parameters.map((p) => p.trim())].flat(),
-      )
-      .map(([type, subtype, ...parameters]) => ({
-        type,
-        subtype,
-        parameters: parameters.filter(notNullOrUndefined),
-      })) ?? []
-  );
-};
-
-const matches = (
-  provide: { type?: string; subtype?: string },
-  accepted: { type?: string; subtype?: string },
-) => {
-  if (provide.type === "*" || accepted.type === "*") {
-    return true;
-  }
-
-  if (provide.type !== accepted.type) {
-    return false;
-  }
-
-  if (
-    provide.subtype === "*" ||
-    accepted.subtype === "*" ||
-    !accepted.subtype
-  ) {
-    return true;
-  }
-
-  if (provide.subtype !== accepted.subtype) {
-    return false;
-  }
-
-  return true;
-};
-
-const filterQueries = (accept?: string) => {
-  const accepts = parseAccept(accept);
-
-  return (q: Queryable) => {
-    const provides = parseAccept(q.mediaType);
-
-    for (const accept of accepts) {
-      for (const provide of provides) {
-        if (matches(provide, accept)) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  };
 };
 
 export const QueryableField = (
